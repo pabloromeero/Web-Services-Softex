0a2cbae1dea26ec98fbf4b5540e847155de25402
Pablo Romero <pablo10romero10@gmail.com>
2022-11-21 08:10:28 -0300

    Create webServices-Soap

---
+++ modulo1/webServices-Soap/main.java
@@ -0,0 +1,29 @@
+/*
+ *  crie a estrutura de um web service, que demonstra exemplos de comunicação SOAP 
+ *  (requisição e resposta em xmls) e explique como ele funciona. 
+ *  Resposta: Criei um Web Services básica utilizando a comunicação SOAP, 
+ *  na qual retornará o Hello e nome do cliente.
+ */
+package pacote1;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Pablo Romero
+ */
+@WebService(serviceName = "Hello")
+@Stateless()
+public class Soma {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
